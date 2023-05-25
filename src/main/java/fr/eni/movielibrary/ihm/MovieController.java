952c1c655b1bb39d1df11f96d43c820e571cd233
Anthony FOURQUIER <afourquier2022@campus-eni.fr>
2023-05-25 16:50:56 +0200

    add edit movie page html

--- src/main/java/fr/eni/movielibrary/ihm/MovieController.java
+++ src/main/java/fr/eni/movielibrary/ihm/MovieController.java
@@ -1,31 +1,21 @@
 package fr.eni.movielibrary.ihm;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.eni.movielibrary.bll.mock.MovieServiceMock;
 import fr.eni.movielibrary.bo.Movie;
 
-@Controller("movieBean")
+@Controller
 public class MovieController {
 	
 	@Autowired
 	private MovieServiceMock movieServiceMock;
-	
-	public Movie findMovie(int i) {
-		// TODO Auto-generated method stub
-		return movieServiceMock.getMovieById(i);
-	}
-	
-	public List<Movie> showAllMovies() {
-		// TODO Auto-generated method stub
-		return movieServiceMock.getAllMovies();
-	}
 	
 	@GetMapping("/")
     public String homePage(Model model) {
@@ -38,5 +28,17 @@ public class MovieController {
 		model.addAttribute("movie", movieServiceMock.getMovieById(id));
         return "detailMovie";
     }
+	
+	@GetMapping("/editMovie/{id}")
+    public String editMoviePage(Model model, @PathVariable("id") int id) {
+		model.addAttribute("movie", movieServiceMock.getMovieById(id));
+        return "editMovie";
+    }
+	
+	@PostMapping("movie")
+	public String personSubmit(@ModelAttribute("formMovie") Movie movie) {
+		System.out.println(String.format("Movie : %s", movie.getTitle()));
+		return "home";
+	}
 	
 }
